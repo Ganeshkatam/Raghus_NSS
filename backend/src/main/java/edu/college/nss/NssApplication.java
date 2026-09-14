3ce442dd915896d6a18cae2f9dc3550a62bb0d54
Katam Ganesh Reddy <katamganesh61@gmail.com>
2026-09-14 22:42:05 +0530

    feat(backend): add Spring Boot entrypoint

---
+++ backend/src/main/java/edu/college/nss/NssApplication.java
@@ -0,0 +1,11 @@
+package edu.college.nss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NssApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NssApplication.class, args);
+    }
+}
